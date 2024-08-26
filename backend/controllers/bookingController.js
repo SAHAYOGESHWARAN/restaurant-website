b10b01f23b9d9599c0fe6cf2bb85dc898620627d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-26 11:39:36 +0530

    RW

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,9 @@
+const Booking = require('../models/bookingModel');
+
+exports.bookRoom = async (req, res) => {
+    // Booking logic
+};
+
+exports.getBookings = async (req, res) => {
+    // Fetch bookings
+};
