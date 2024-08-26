5deb99f2c0c1e4fe796e60de7ec76a039fc5e6c6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-26 11:39:12 +0530

    RW

---
+++ backend/controllers/authController.js
@@ -0,0 +1,10 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+exports.register = async (req, res) => {
+    // Registration logic
+};
+
+exports.login = async (req, res) => {
+    // Login logic
+};
